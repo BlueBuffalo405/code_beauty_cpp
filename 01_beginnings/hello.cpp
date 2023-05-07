735d973ef881dd7a4c67ca84978427882b75b38c
BlueBuffalo405 <fioren.hollif@gmail.com>
2023-05-07 06:43:53 +0000

    My First Commit

---
+++ 01_beginnings/hello.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>		// For the C Library "printf()"
+#include <iostream>		// For standard C++ input/output.
+
+int main()
+{
+	std::cout << "<iostream> Hello CPP World!\n";
+
+	printf("<cstdio> TEST\n");
+}
